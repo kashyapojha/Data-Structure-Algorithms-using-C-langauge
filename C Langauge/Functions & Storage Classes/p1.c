b94d78b47d68ab4d8fdf9d96c428e6a0d198927b
Kashyap Ojha <143380441+kashyapojha@users.noreply.github.com>
2024-11-11 06:39:13 +0530

    Add files via upload

--- C Langauge/Functions & Storage Classes/p1.c
+++ C Langauge/Functions & Storage Classes/p1.c
@@ -1,7 +1,70 @@
-int f(int n){
-    static int i=1;
-    if(n>=5)
-    return n;
-n=n+i;
-r
-}
+// #include<stdio.h>
+// int fun(int x, int y ){    // function declaration 
+//     return x+y;
+// }
+
+// int main(){
+//     int i=10, j=20, k;
+//     k= fun(i,j);                    // function call 
+//     printf("%d",k);
+//     return 0;
+// };
+
+// #include<stdio.h>
+// void foo(int, int);
+// int main(){
+//     int a=10, b=20;
+//     foo(a,b);
+//     printf("\n%d%d",a,b);
+//     return 0;
+// };
+// void foo(int a , int b){
+//     int temp;
+//     temp=a;
+//     a=b;
+//     b=temp;
+//     printf("%d%d",a,b);
+// };
+
+// #include<stdio.h>
+// void a(){
+//     printf("a"); b();
+// }
+// void b(){
+//     printf("a"); c();
+// }
+// void c(){
+//     printf("a");
+// }
+// int main(){
+//     a();
+//     b();
+//     return 0;
+// };
+
+// #include<stdio.h>
+// int g(int p){
+//     printf("%d",p); return p;
+// }
+// int h(int q){
+//     printf("%d",q); return q;
+// }
+// void f(int x, int y){
+//     g(x);
+//     h(y);
+// }
+// int main(){
+//     f(g(10),h(20));    //evaluating right to left 
+// }
+// #include<stdio.h>
+// int f(int n){
+//     static int i=1;
+//     if(n>=5)
+//     return n;
+// n=n+i;
+// i++;
+// return f(n);
+// };
+// int main(){
+//     printf("%d", f(1));
+// }
